5383a6cdcd0b71ae377a364636352f667aad8422
esnoeijs <emmasnoeijs@gmail.com>
2020-11-30 23:39:42 +0100

    did day5 part 2 without actually cleaning up the code. Given that AoC 2020 starts tomorrow I don't think I'll get around to cleaning this up.

--- src/day5.rs
+++ src/day5.rs
@@ -6,7 +6,10 @@ pub fn solution(filename: &String) {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
     let mut prog = Program::from(contents.as_str());
-    println!("{:?}", prog.execute("1"));
+    println!("a: {:?}", prog.execute("1"));
+
+    let mut prog = Program::from(contents.as_str());
+    println!("b: {:?}", prog.execute("5"));
 }
 
 struct Program {
@@ -43,6 +46,10 @@ impl Program {
                 2 => Multiplication::apply(self),
                 3 => Input::apply(self),
                 4 => Output::apply(self),
+                5 => Jump::apply(self, true),
+                6 => Jump::apply(self, false),
+                7 => Compare::apply(self, '<'),
+                8 => Compare::apply(self, '='),
                 99 => return self.output.clone(),
                 _ => panic!("unknown opcode"),
             }
@@ -103,6 +110,10 @@ struct Input {}
 
 struct Output {}
 
+struct Jump {}
+
+struct Compare {}
+
 impl Multiplication {
     fn apply(program: &mut Program) {
         let arg_modes = ArgumentModes::new(program.opcodes[&program.index]);
@@ -138,16 +149,21 @@ impl Input {
         let arg_modes = ArgumentModes::new(program.opcodes[&program.index]);
         program.next();
         let position = program.opcodes[&program.index];
+
+        let input = program.input.parse();
+        if input.is_err() {
+            panic!("input required");
+        }
+
         println!(
-            "{:?} Input {:?} {:?}",
+            "idx: {:?} op: Input value: {:?} write to position:{:?} {:?}",
             program.index - 1,
+            input,
             position,
             arg_modes
         );
 
-        program
-            .opcodes
-            .insert(position as usize, program.input.parse().unwrap());
+        program.opcodes.insert(position as usize, input.unwrap());
         program.next();
     }
 }
@@ -163,19 +179,86 @@ impl Output {
         };
 
         program.next();
+
+        let value: i32 = program.opcodes[&(position as usize)];
         println!(
-            "{:?} Output {:?} {:?}",
-            program.index - 2,
+            "idx: {:?} op: Output value: {:?} from position:{:?} {:?}",
+            program.index - 1,
+            value,
             position,
             arg_modes
         );
-
-        let value: i32 = program.opcodes[&(position as usize)];
 
         program
             .output
             .write_str(format!("{}\n", value.to_string()).as_str())
             .unwrap();
+    }
+}
+
+impl Jump {
+    fn apply(program: &mut Program, is_not_zero: bool) {
+        let arg_modes = ArgumentModes::new(program.opcodes[&program.index]);
+        program.next();
+        let arg_a = arg_modes.get_value(0, program.index, &program.opcodes);
+        program.next();
+        let arg_b = arg_modes.get_value(1, program.index, &program.opcodes);
+        program.next();
+
+        println!(
+            "idx: {:?} op: Jump if zero {:?} a: {:?} jump to idx: {:?} {:?}",
+            program.index - 1,
+            is_not_zero,
+            arg_a,
+            arg_b,
+            arg_modes
+        );
+
+        if is_not_zero == true && arg_a != 0 || is_not_zero == false && arg_a == 0 {
+            program.index = arg_b as usize;
+        }
+    }
+}
+
+macro_rules! ternary {
+    ($c:expr, $v:expr, $v1:expr) => {
+        if $c {
+            $v
+        } else {
+            $v1
+        }
+    };
+}
+
+impl Compare {
+    fn apply(program: &mut Program, operator: char) {
+        let arg_modes = ArgumentModes::new(program.opcodes[&program.index]);
+        program.next();
+        let arg_a = arg_modes.get_value(0, program.index, &program.opcodes);
+        program.next();
+        let arg_b = arg_modes.get_value(1, program.index, &program.opcodes);
+        program.next();
+        let position = program.opcodes[&program.index];
+
+        let value = match operator {
+            '<' => ternary!(arg_a < arg_b, 1, 0),
+            '=' => ternary!(arg_a == arg_b, 1, 0),
+            _ => panic!("unknown compare mode"),
+        };
+
+        println!(
+            "idx: {:?} op: Compare mode {:?} a: {:?} b: {:?} write {:?} to pos {:?} {:?}",
+            program.index - 1,
+            operator,
+            arg_a,
+            arg_b,
+            value,
+            position,
+            arg_modes
+        );
+
+        program.opcodes.insert(position as usize, value);
+        program.next();
     }
 }
 
@@ -239,5 +322,56 @@ mod tests {
         assert_eq!(1, arg_modes.get_mode(0));
         assert_eq!(0, arg_modes.get_mode(1));
         assert_eq!(1, arg_modes.get_mode(2));
+    }
+
+    #[test]
+    fn test_day5_b_compare() {
+        // positional mode, equals
+        let mut prog = Program::from("3,9,8,9,10,9,4,9,99,-1,8");
+        assert_eq!("1\n", prog.execute("8"));
+
+        let mut prog = Program::from("3,9,8,9,10,9,4,9,99,-1,8");
+        assert_eq!("0\n", prog.execute("1"));
+
+        // immediate mode, equals
+        let mut prog = Program::from("3,3,1108,-1,8,3,4,3,99");
+        assert_eq!("1\n", prog.execute("8"));
+
+        let mut prog = Program::from("3,3,1108,-1,8,3,4,3,99");
+        assert_eq!("0\n", prog.execute("1"));
+
+        // positional mode, less then
+        let mut prog = Program::from("3,9,7,9,10,9,4,9,99,-1,8");
+        assert_eq!("1\n", prog.execute("7"));
+
+        let mut prog = Program::from("3,9,7,9,10,9,4,9,99,-1,8");
+        assert_eq!("0\n", prog.execute("9"));
+
+        // immediate mode, less then
+        let mut prog = Program::from("3,3,1107,-1,8,3,4,3,99");
+        assert_eq!("1\n", prog.execute("7"));
+
+        let mut prog = Program::from("3,3,1107,-1,8,3,4,3,99");
+        assert_eq!("0\n", prog.execute("9"));
+    }
+
+    #[test]
+    fn test_day5_b_jump() {
+        let mut prog = Program::from("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9");
+        assert_eq!("0\n", prog.execute("0"));
+        let mut prog = Program::from("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9");
+        assert_eq!("1\n", prog.execute("5"));
+    }
+
+    #[test]
+    fn test_day5_b() {
+        let mut prog = Program::from("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99");
+        assert_eq!("999\n", prog.execute("7"));
+
+        let mut prog = Program::from("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99");
+        assert_eq!("1000\n", prog.execute("8"));
+
+        let mut prog = Program::from("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99");
+        assert_eq!("1001\n", prog.execute("9"));
     }
 }
